4a95e6c662520841b8b3e367e25c8feb670e0c61
Elliot Forcier-Poirier <elliot.forcier-poirier@mail.mcgill.ca>
2023-05-15 16:06:19 -0400

    Finished the display of the Clicked Issue Type Component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -387,7 +387,7 @@ export default function Home() {
 
               {/** Clicked Issue Type Component */}
               <div
-                className="flex flex-col rounded-lg border bg-gray-200/50 w-full h-48 ">
+                className="flex flex-col rounded-lg border bg-gray-200/50 w-full h-48 gap-1">
                 <div className='flex flex-row w-full h-16 justify-top'>
                   <div className='flex flex-row rounded-lg border bg-gray-200 w-full justify-between transition-colors hover:border-gray-300 hover:dark:bg-neutral-800/30 '>
                     <div className='flex items-center justify-start w-5/6 overflow-x-clip'>
@@ -416,8 +416,8 @@ export default function Home() {
                     </div>
                   </div>
                 </div>
-                <div className='relative flex mx-1 items-center justify-center h-5/6 m-2 gap-2'>
-                  <div className='flex flex-col gap-2 overflow-y-scroll overflow-x-clip w-full h-full'>
+                <div className='relative flex mx-1 items-center justify-center h-32'>
+                  <div className='flex flex-col gap-2 overflow-y-scroll overflow-x-clip w-full h-full m-2'>
 
                     {/** List of the actual issues of this specific type */}
 
@@ -561,24 +561,30 @@ export default function Home() {
               </div>
 
               {/** End of  */}
-              <div className='flex flex-row rounded-lg border bg-gray-200 w-full h-16 justify-between transition-colors hover:border-gray-300 hover:dark:bg-neutral-800/30'>
-                <div className='flex flex-row m-2 items-center justify-center'>
-                  <div className='flex h-full justify-start mx-1 items-center'>
-                    <h2 className='text-xl font-semibold'>Type</h2>
+              <div className='flex flex-row w-full h-16 justify-top'>
+                <div className='flex flex-row rounded-lg border bg-gray-200 w-full justify-between transition-colors hover:border-gray-300 hover:dark:bg-neutral-800/30 '>
+                  <div className='flex items-center justify-start w-5/6 overflow-x-clip'>
+                    <div className='flex flex-row m-2 justify-start items-center'>
+                      <div className='flex h-full justify-start mx-1 items-center'>
+                        <h2 className='text-xl font-semibold'>Type</h2>
+                      </div>
+                      <div className='flex h-full justify-start mx-1 items-center'>
+                        <h2 className='text-xl font-light'> | </h2>
+                      </div>
+                      <div className='flex items-center justify-start'>
+                        <p className='text-sm font-light text-left truncate'>
+                          This error occurs when there is missing alt-text on the page
+                        </p>
+                      </div>
+                    </div>
                   </div>
-                  <div className='flex h-full justify-start mx-1 items-center'>
-                    <h2 className='text-xl font-light'> | </h2>
-                  </div>
-                  <div className='flex items-center justify-start'>
-                    <p className='text-sm font-light text-left truncate'>
-                      This error occurs when there is missing alt-text on the page
-                    </p>
-                  </div>
-                </div>
-                <div className='flex rounded-lg border bg-gray-300 m-2 w-1/6 justify-center items-center flex-shrink-0'>
-                  <div className='flex h-full m-1 items-center justify-between'>
-                    <div className='flex h-full items-center justify-start mx-1'>
-                      <p className='text-xl font-bold'>{errorCount}</p>
+                  <div className='flex w-1/6 justify-center items-center flex-shrink-0'>
+                    <div className='flex rounded-lg border bg-gray-300 m-2 w-full items-center justify-center'>
+                      <div className='flex h-full m-1 items-center justify-between'>
+                        <div className='flex h-full items-center justify-start mx-1'>
+                          <p className='text-xl font-bold'>{errorCount}</p>
+                        </div>
+                      </div>
                     </div>
                   </div>
                 </div>
