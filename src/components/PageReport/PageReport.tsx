3ac54d66358c13ed0f9dee574574b890fdeb3a0a
Elliot Forcier-Poirier <elliot.forcier-poirier@mail.mcgill.ca>
2023-07-10 13:22:06 -0400

    fix error in issueButton display in PageReport.tsx

--- src/components/PageReport/PageReport.tsx
+++ src/components/PageReport/PageReport.tsx
@@ -1,3 +1,4 @@
+
 import { FiAlertCircle } from 'react-icons/fi'
 import { FiAlertTriangle } from 'react-icons/fi'
 import { ImTree } from 'react-icons/im'
@@ -10,10 +11,10 @@ import { CircularProgress } from '@mui/material'
 import { PageSearch } from '../PageSearch';
 
 const colorScheme = {
-    'error': { 'text': '#EA0404', bg: '#FED7D7', border: '#FDB0B0', hover: '#FEEBEB' },
-    'contrast': { 'text': '#008AE0', bg: '#C2E8FF', border: '#70C8FF', hover: '#C2E8FF' },
-    'structure': { 'text': '#2CB56E', bg: '#CEF3E0', border: '#5BD797', hover: '#CEF3E0' },
-    'alert': { 'text': '#FF9505', bg: '#FFE6C2', border: '#FFCC85', hover: '#FFEFD6' },
+    'error': { text: '#EA0404', bg: 'bg-[#FED7D7]', border: 'border-[#FDB0B0]', 'hover': 'hover:bg-[#FEEBEB]' },
+    'contrast': { 'text': '#008AE0', bg: 'bg-[#C2E8FF]', border: 'border-[#70C8FF]', hover: 'hover:bg-[#C2E8FF]' },
+    'structure': { 'text': '#2CB56E', bg: 'bg-[#CEF3E0]', border: 'border-[#5BD797]', hover: 'hover:bg-[#CEF3E0]' },
+    'alert': { 'text': '#FF9505', bg: 'bg-[#FFE6C2]', border: 'border-[#FFCC85]', hover: 'hover:bg-[#FFEFD6]' },
 };
 
 const issueIcons = {
@@ -40,7 +41,7 @@ export const PageReport = () => {
         const Icon = (issueIcons as any)[type];
 
         return (
-            <button onClick={() => { handleSelectedIssueState(type) }} className={`flex flex-row w-full h-10 items-center justify-start hover:bg-[${issueColorScheme.hover}] px-1`}
+            <button onClick={() => { handleSelectedIssueState(type) }} className={`flex flex-row w-full h-10 items-center justify-start ${issueColorScheme.hover} px-1`}
                 style={{
                     backgroundColor: SelectedIssueState.selected == type ? 'white' : '',
                     borderRight: SelectedIssueState.selected == type ? '2px solid blue' : '',
@@ -55,8 +56,8 @@ export const PageReport = () => {
                     </div>
                 </div>
                 <div className='flex shrink-0 w-1/3 h-full p-2 justify-center items-center'>
-                    <div className={`flex w-full h-full border-2 border-[${issueColorScheme.border}] bg-[${issueColorScheme.bg}] rounded items-center justify-center`}>
-                        {WebsiteReport.isLoading ? <span className='text-l items-center justify-center h-full font-semibold text-[#EA0404]'> <CircularProgress sx={{ color: issueColorScheme.text }} style={{ width: 16, height: 16 }} />  </span> : <span className={`text-l items-center justify-center font-semibold text-[${issueColorScheme.text}]`}> {PageStoreState.error.count} </span>}
+                    <div className={`flex w-full h-full border-2 ${issueColorScheme.border} ${issueColorScheme.bg} rounded items-center justify-center`}>
+                        {WebsiteReport.isLoading ? <span className='text-l items-center justify-center h-full font-semibold text-[#EA0404]'> <CircularProgress sx={{ color: issueColorScheme.text }} style={{ width: 16, height: 16 }} />  </span> : <span className={`text-l items-center justify-center font-semibold text-[${issueColorScheme.text}]`}> {(PageStoreState as any)[type].count} </span>}
                     </div>
                 </div>
             </button>
@@ -93,7 +94,7 @@ export const PageReport = () => {
                                                 className="flex flex-col w-full h-full justify-center items-center">
                                                 <div className='flex flex-col w-full h-16 items-center justify-start'>
                                                     <div className='flex flex-row w-full h-8 items-center justify-start'>
-                                                        <h2 className='font-semibold text-xl'> {(PageStoreState as any)[SelectedIssueState.selected].description} Report </h2>
+                                                        <h2 className='font-semibold text-xl truncate'> {(PageStoreState as any)[SelectedIssueState.selected].description} Report </h2>
                                                     </div>
                                                     <div className='flex flex-row w-full h-4 items-center justify-start text-sm'>
                                                         <p className='text-xs font-extralight text-gray-400 justify-start items-center'> {(PageStoreState as any)[SelectedIssueState.selected].description} </p>
